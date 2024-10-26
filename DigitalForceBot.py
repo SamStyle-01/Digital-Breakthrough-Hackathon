c460ba3fe97b82161f8eaf2a69b8d06ad7a840c9
Sam <115551276+SamStyle-01@users.noreply.github.com>
2024-10-27 01:22:16 +0300

    Add files via upload

--- DigitalForceBot.py
+++ DigitalForceBot.py
@@ -10,10 +10,76 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+from docx import Document
+from openpyxl import load_workbook
 
-TOKEN = # Вставить токен здесь
+TOKEN = # Вставить токен
 dp = Dispatcher()
+mime_type = None
 times = 0
+
+
+def read_docx(file_content):
+    document = Document(file_content)
+    text = []
+    for paragraph in document.paragraphs:
+        text.append(paragraph.text)
+
+    return "\n".join(text)
+
+
+def read_excel(file_content):
+    workbook = load_workbook(filename=file_content)
+    sheet = workbook.active
+
+    data = []
+    for row in sheet.iter_rows(values_only=True):
+        data.append(list(row))
+
+    return data
+
+
+@dp.message(F.document)
+async def handle_document(message: Message, bot: Bot):
+    global mime_type
+    document = message.document
+    file_name = document.file_name
+    mime_type = document.mime_type
+
+    if mime_type in [
+        "application/pdf",
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    ]:
+        file_data = await bot.download(document.file_id)
+
+        await message.reply(f"Файл '{file_name}' получен и сохранен в переменную.")
+
+        if mime_type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+            await message.reply(f"{read_docx(file_data)[:300]}")
+        elif mime_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+            ex_file = read_excel(file_data)
+
+            rex_file = ""
+            for i in range(len(ex_file)):
+                for j in range(len(ex_file[i])):
+                    try:
+                        rex_file = rex_file + ex_file[i][j] + ", "
+                    except TypeError:
+                        pass
+                rex_file = rex_file + "\n"
+            await message.reply(f"{rex_file[:300]}")
+    else:
+        await message.reply("Пожалуйста, отправьте файл в формате PDF, DOCX или XLSX.")
+
+
+def save_audio_file(audio_bytes, audio_format="ogg"):
+    global times
+    times += 1
+    output_path = fr"C:\Users\Sam\Desktop\output{times}.wav"
+    audio = AudioSegment.from_file(BytesIO(audio_bytes), format=audio_format)
+    audio.export(output_path, format="wav")
+    print(f"Файл успешно сохранён по пути: {output_path}")
 
 
 def recognize_speech_from_audio(audio_bytes, audio_format="ogg"):
@@ -21,10 +87,10 @@ def recognize_speech_from_audio(audio_bytes, audio_format="ogg"):
 
     wav_io = BytesIO()
     audio.export(wav_io, format="wav")
-    wav_io.seek(0)  # Обнуляем указатель для дальнейшего чтения
+    wav_io.seek(0)
+    save_audio_file(audio_bytes)
 
 
-    # Используем SpeechRecognition для распознавания речи
     recognizer = sr.Recognizer()
     text = ""
     with sr.AudioFile(wav_io) as source:
@@ -42,22 +108,10 @@ def recognize_speech_from_audio(audio_bytes, audio_format="ogg"):
 
 @dp.message(F.voice)
 async def handle_voice(message: Message, bot: Bot):
-    # Получаем аудиофайл и передаем его для распознавания
     voice = await bot.download(message.voice.file_id)
     audio_bytes = voice.read()
     recognized_text = recognize_speech_from_audio(audio_bytes)
     await message.reply(recognized_text)
-
-# Другие обработчики и функции...
-
-async def main():
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
-
 
 
 @dp.message()
@@ -68,7 +122,6 @@ async def echo_handler(message: Message) -> None:
         await message.answer("Nice try!")
 
 
-# Команда /start
 @dp.message(CommandStart())
 async def command_start_handler(message: Message):
     await message.answer(
